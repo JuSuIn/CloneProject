4881004ec867aa0298a01cfa162aec2f90526e56
SUINJU <jusuin5876@naver.com>
2025-01-16 00:51:29 +0900

    sqllite3 DB Table and Django Model Create Add CONTENT_FEED

--- content/models.py
+++ content/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+# Justagram need DB Modeling
+
+#Justagram feed
+class Feed(models.Model):
+    content = models.TextField() #TextDetail(글내용)
+    image = models.TextField()  #FeedImage(피드이미지)
+    profile_image = models.TextField() #profileimage(프로필이미지)
+    user_id = models.IntegerField() #Author(글쓴이)
+    like_count = models.IntegerField() #like count(좋아요 수)
+
+
+
+
