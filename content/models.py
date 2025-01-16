4c76af959c94d03f64cd25f4073097cf74879d51
SUINJU <jusuin5876@naver.com>
2025-01-16 19:44:58 +0900

    장고 ORM을 이용해 sqlife5 와 feed 화면  DB 기능 구현(Implement sqlife5 and feed screen DB functions using Django ORM)

--- content/models.py
+++ content/models.py
@@ -9,7 +9,7 @@ class Feed(models.Model):
     content = models.TextField() #TextDetail(글내용)
     image = models.TextField()  #FeedImage(피드이미지)
     profile_image = models.TextField() #profileimage(프로필이미지)
-    user_id = models.IntegerField() #Author(글쓴이)
+    user_id = models.TextField() #Author(글쓴이)
     like_count = models.IntegerField() #like count(좋아요 수)
 
 
--- content/views.py
+++ content/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from .models import Feed
 
 # Create your views here.
+class Main(APIView):
+    def get(self, request):
+        feed_list = Feed.objects.all().order_by('-id') # select * from content_feed; , querySet
+        # print(feed_list)
+        # feed print
+        # for feed in feed_list:
+        #     print(feed.content)
+        return render(request,"Justagram/main.html",context=dict(feeds=feed_list))
+
+
