6120bee2f66c63258cb72c6b2eb6f8abeeb2c6ff
SUINJU <jusuin5876@naver.com>
2025-01-19 14:33:58 +0900

    클론 코딩 인스타그램 피드 부분 공유하기에 사용 되는 파일 업로드 기능 추가(ajax,Django media 등 활용) (Added file upload function used to share clone coding Instagram feed portion (using ajax, Django media, etc.))

--- Justagram/urls.py
+++ Justagram/urls.py
@@ -15,12 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-# from django.contrib import admin
+from django.contrib import admin
 from django.urls import path
 from .views import Sub
-from content.views import Main
-
+from content.views import Main,UploadFeed
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
-    #    path("admin/", admin.site.urls),
+    path("admin/", admin.site.urls),
     path('main/',Main.as_view()),
+    path('content/upload',UploadFeed.as_view()),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- content/views.py
+++ content/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Feed
+import os
+from Justagram.settings import MEDIA_ROOT
+from uuid import uuid4
 
 # Create your views here.
 class Main(APIView):
@@ -11,5 +15,29 @@ class Main(APIView):
         # for feed in feed_list:
         #     print(feed.content)
         return render(request,"Justagram/main.html",context=dict(feeds=feed_list))
+
+#file upload
+class UploadFeed(APIView):
+    def post(self, request):
+        file = request.FILES['file']
+
+        uuid_name = uuid4().hex
+        save_path = os.path.join(MEDIA_ROOT, uuid_name)
+
+        with open(save_path, 'wb+') as destination:
+            for chunk in file.chunks():
+                destination.write(chunk)
+
+        # file = request.data.get('file')
+        image =uuid_name
+        # image =request.data.get('image')
+        content =request.data.get('content')
+        user_id =request.data.get('user_id')
+        profile_image =request.data.get('profile_image')
+
+        Feed.objects.create(image=image,content=content,user_id=user_id,profile_image=profile_image,like_count=0)
+
+        return Response(status=200)
+
 
 
