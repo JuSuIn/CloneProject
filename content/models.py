6d3573f927d96535dc18813b68c1aab9040a56aa
SUINJU <jusuin5876@naver.com>
2025-01-27 01:12:49 +0900

    프로필 닉네임  구조 변경 및 피드별 댓글 기능 구현 (Changed profile nickname structure and implemented comment function for each feed)

--- content/models.py
+++ content/models.py
@@ -8,9 +8,29 @@ from django.db import models
 class Feed(models.Model):
     content = models.TextField() #TextDetail(글내용)
     image = models.TextField()  #FeedImage(피드이미지)
-    profile_image = models.TextField() #profileimage(프로필이미지)
-    user_id = models.TextField() #Author(글쓴이)
-    like_count = models.IntegerField() #like count(좋아요 수)
+   # profile_image = models.TextField(default='') #profileimage(프로필이미지)
+   # nickname= models.TextField(default='')  #Author(글쓴이)
+    # user_id = models.TextField() #Author(글쓴이)
+    email = models.EmailField(default='') #email(이메일)
+    like_count = models.IntegerField(default=0) #like count(좋아요 수)
+
+# like count
+class Like(models.Model):
+   feed_id = models.IntegerField(default=0)
+   email = models.EmailField(default='')
+   is_like = models.BooleanField(default=True) #update 위한 flag
+
+#comment
+class Reply(models.Model):
+    feed_id = models.IntegerField(default=0)
+    email = models.EmailField(default='')
+    reply_content = models.TextField()
+
+#bookmark
+class BookMark(models.Model):
+    feed_id = models.IntegerField(default=0)
+    email = models.EmailField(default='')
+    is_marked = models.BooleanField(default=False)
 
 
 
--- content/views.py
+++ content/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .models import Feed
+from .models import Feed,Reply,Like,BookMark
 import os
 from Justagram.settings import MEDIA_ROOT
 from uuid import uuid4
@@ -11,16 +11,46 @@ from user.models import User
 # Create your views here.
 class Main(APIView):
     def get(self, request):
-        feed_list = Feed.objects.all().order_by('-id') # select * from content_feed; , querySet
+
+        feed_object_list = Feed.objects.all().order_by('-id') # select * from content_feed; , querySet
+        feed_list = []
+
+        email=''
+        user=''
+
+        for feed in feed_object_list:
+            email=feed.email
+            user=User.objects.filter(email=email).first()
+
+            #reply list function add
+            reply_object_list = Reply.objects.filter(feed_id=feed.id)
+            reply_list = []
+            for reply in reply_object_list:
+                user = User.objects.filter(email=reply.email).first()
+
+                reply_list.append(dict(reply_content=reply.reply_content,
+                                       nickname=user.nickname))
+
+            #print('찾는지 테스트',user.nickname)
+            feed_list.append(dict(id=feed.id,
+                                  content=feed.content,
+                                  image=feed.image,
+                                  profile_image=user.profile_image,
+                                  nickname=user.nickname,
+                                  likes_count=feed.like_count,
+                                  reply_list=reply_list
+                                  ))
+
+        email = request.session.get('email',None) #request.session['email'] # session
+        user = User.objects.filter(email=email).first() # now login user information
+
+
         # print(feed_list)
         # feed print
         # for feed in feed_list:
         #     print(feed.content)
 
        # print(" 로그인한 사용자 : ",request.session['email'])
-
-        email = request.session.get('email',None) #request.session['email'] # session
-        user = User.objects.filter(email=email).first() # now login user information
 
         #print("확실히 제대로 되고 있는건가?!",user.profile_image)
         if email is None:
@@ -47,10 +77,12 @@ class UploadFeed(APIView):
         image =uuid_name
         # image =request.data.get('image')
         content =request.data.get('content')
-        user_id =request.data.get('user_id')
-        profile_image =request.data.get('profile_image')
+        #user_id =request.data.get('user_id')
+        #profile_image =request.data.get('profile_image')
+        email=request.session.get('email',None)
 
-        Feed.objects.create(image=image,content=content,user_id=user_id,profile_image=profile_image,like_count=0)
+        Feed.objects.create(image=image,content=content,email=email)
+        #Feed.objects.create(image=image,content=content,user_id=user_id,profile_image=profile_image,like_count=0)
 
         return Response(status=200)
 
@@ -69,4 +101,13 @@ class Profile(APIView):
 
         return render(request,"content/profile.html",context=dict(user=user))
 
+class UploadReply(APIView):
+    def post(self, request):
+        feed_id = request.data.get('feed_id', None)
+        reply_content = request.data.get('reply_content', None)
 
+        email = request.session.get('email', None)  # request.session['email'] # session
+
+        Reply.objects.create(feed_id=feed_id,reply_content=reply_content,email=email)
+
+        return Response(status=200)
